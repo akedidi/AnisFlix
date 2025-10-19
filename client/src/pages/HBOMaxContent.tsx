027b553c3c2bbaf614e588b6d06826e40697ef8f
Anis Kedidi <akedidi@fr.ibm.com>
2025-10-20 00:04:30 +0200

    🐛 Correction finale de la syntaxe JSX dans HBOMaxContent.tsx
    🔧 Problème corrigé:
    - Code orphelin 'showSearch={true}' et '>' supprimé
    - Structure JSX maintenant valide
    - Plus d'erreurs de build Vercel
    
    ✨ Résultat:
    - Build Vercel fonctionne maintenant
    - Syntaxe JSX complètement corrigée
    - Pull-to-refresh fonctionne correctement

--- client/src/pages/HBOMaxContent.tsx
+++ client/src/pages/HBOMaxContent.tsx
@@ -80,8 +80,6 @@ export default function HBOMaxContent() {
 
 
       <PullToRefresh onRefresh={handleRefresh}>
-      showSearch={true}
-    >
 
       {/* Header */}
       <div className="relative bg-gradient-to-b from-primary/20 to-background">
